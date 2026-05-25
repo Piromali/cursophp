7687a03989d0c2174c27794fa45966879b7c68ec
Clara <claraprogf@gmail.com>
2026-05-25 00:24:11 -0300

    25/05/26

---
+++ exemplos/casesensitive/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $nome = "Pedro";
+        $Nome = "Maria";
+
+        echo "É verdade que seu nome é $nome?";
+        echo "Na verdade seu nome é $Nome";
+
+        //case sensitive
+    ?>
+</body>
+</html>
