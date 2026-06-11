25c8f2f7bb44785842ed74008bbc7c6f69489931
Clara <claraprogf@gmail.com>
2026-06-11 17:56:01 -0300

    analisador de numero real

--- desafios/conversor_de_moedasv2/conv.php
+++ desafios/conversor_de_moedasv2/conv.php
@@ -30,7 +30,7 @@
             $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
             
             echo "<p>Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrao, $dolar, "USD") . "</strong></p>";
-            echo "<p>* Cotação ($cotação) obtida diretamente no site do <strong><a href='https://www.bcb.gov.br/'>Banco central do Brasil</a></p>"
+            echo "<p>* Cotação (<i>$cotação</i>) obtida diretamente no site do <strong><a href='https://www.bcb.gov.br/'>Banco central do Brasil</a></p>"
         ?>
         <button onclick="javascript:history.go(-1)">&#x2B05; Voltar</button>
     </main>
